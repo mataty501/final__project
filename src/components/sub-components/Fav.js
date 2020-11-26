67e3a1487eee15caede7166eafaecf77199842a7
Walid <alioua.walid1993@gmail.com>
2020-11-26 15:28:57 +0100

    8

--- src/components/sub-components/Fav.js
+++ src/components/sub-components/Fav.js
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux';
 
 const Fav = (props) => {
   const fav_list = useSelector(state => state.Fav.fav)
-  const [empty, setEmpty] = useState(false)
+
 
 
 
@@ -17,33 +17,33 @@ const Fav = (props) => {
           <Modal.Title>Favorite List</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-
-          {fav_list.length && <Table striped bordered hover size="sm">
-            <thead>
-              <tr>
-
-                <th>Product name</th>
-                <th className="delete-trash-container">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-
-
-              {fav_list.map((e) => {
-
-                return (<tr>
-
-                  <td>{e.name}</td>
-                  <th className="delete-trash-container"><FaTrashAlt className="delete-trash" /></th>
-                </tr>)
-              })
-              }
-
-            </tbody>
-          </Table>}
-
-          {!fav_list.length && <div className="fav-empty-msg">List of favorite product is currently empty</div>}
-
+          {!!fav_list.length && (
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>Product name</th>
+                  <th className="delete-trash-container">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {fav_list.map((e) => {
+                  return (
+                    <tr>
+                      <td>{e.name}</td>
+                      <th className="delete-trash-container">
+                        <FaTrashAlt className="delete-trash" />
+                      </th>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          )}
+          {!!!fav_list.length && (
+            <div className="fav-empty-msg">
+              List of favorite product is currently empty
+            </div>
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={(e) => props.close(e)}>
